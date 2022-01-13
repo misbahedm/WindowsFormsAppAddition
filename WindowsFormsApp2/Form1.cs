1e263e5f2900f3bc671cbb3efcbad8e57155c25d
Mohiminul Islam <misbah129@gmail.com>
2022-01-13 16:16:20 +0600

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int sum = a + b;
+            textBox3.Text = sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure want to exit?","Exit",MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Information)==DialogResult.Yes)
+                    {
+                Application.Exit();
+            }
+        }
+    }
+}
